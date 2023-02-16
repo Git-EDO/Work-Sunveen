bb12f2ecb025a9e6cf7eb7d7b7183925db1dbfdb
Dmitry Ezhov <107861036+Git-EDO@users.noreply.github.com>
2023-02-16 16:53:02 +0400

    product

--- js/script.js
+++ js/script.js
@@ -612,45 +612,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Создание метатегов для продукта
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (window.location.pathname.includes('product')) {
-    const head = document.querySelector('head')
-    const productName = document.querySelector(
-      'h2.product-description-name'
-    ).innerText
-    const productPrice = document.querySelector(
-      '.product-description-new-price'
-    ).innerText
+// document.addEventListener('DOMContentLoaded', () => {
+//   if (window.location.pathname.includes('product')) {
+//     const head = document.querySelector('head')
+//     const productName = document.querySelector(
+//       'h2.product-description-name'
+//     ).innerText
+//     const productPrice = document.querySelector(
+//       '.product-description-new-price'
+//     ).innerText
 
-    const metaInfo = `
-    <title itemprop="headline">
-    ${productName} купить по цене ${productPrice} в Калининграде — интернет магазин sunveen.ru 
-    </title>
-      <meta
-        itemprop="description"
-        name="description"
-        ${productName} в интернет-магазине sunveen.ru   всего за ${productPrice}. Доставка по РФ, скидки, акции. Звоните: +7 963 738-42-40
-    />
-    <meta
-    itemprop="keywords"
-    name="keywords"
-    content="${productName}, ${productName} цена, ${productName} купить"
-/>
-    `
-    head.innerHTML += metaInfo
+//     const metaInfo = `
+//     <title itemprop="headline">
+//     ${productName} купить по цене ${productPrice} в Калининграде — интернет магазин sunveen.ru
+//     </title>
+//       <meta
+//         itemprop="description"
+//         name="description"
+//         ${productName} в интернет-магазине sunveen.ru   всего за ${productPrice}. Доставка по РФ, скидки, акции. Звоните: +7 963 738-42-40
+//     />
+//     <meta
+//     itemprop="keywords"
+//     name="keywords"
+//     content="${productName}, ${productName} цена, ${productName} купить"
+// />
+//     `
+//     head.innerHTML += metaInfo
 
-    const miniImages = document.querySelectorAll('.product-mini-item img')
-    const zoomImage = document.querySelector('.products-zoom-img > img')
+//     const miniImages = document.querySelectorAll('.product-mini-item img')
+//     const zoomImage = document.querySelector('.products-zoom-img > img')
 
-    if (miniImages.length) {
-      miniImages.forEach((image) => {
-        image.alt = productName
-        image.title = productName
-      })
-    }
-    if (zoomImage) {
-      zoomImage.alt = productName
-      zoomImage.title = productName
-    }
-  }
-})
+//     if (miniImages.length) {
+//       miniImages.forEach((image) => {
+//         image.alt = productName
+//         image.title = productName
+//       })
+//     }
+//     if (zoomImage) {
+//       zoomImage.alt = productName
+//       zoomImage.title = productName
+//     }
+//   }
+// })
